b5ee30c8d745e78e8ff034fe4e49b1e9aded0237
Olajuwon Lawal <olajuwon.lawal@pmglobaltechnology.com>
2023-05-29 20:21:32 +0100

    Fix: axios error

--- trucki-ui/src/components/Auth/LoginPage.jsx
+++ trucki-ui/src/components/Auth/LoginPage.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import axios from 'axios';
 import Logo from '../../assets/Logo.svg';
 import LoginImage from '../../assets/loginImage.png';
 import '../../App.css';
 import TextSlider from '../TextSlider/TextSlider';
+import  axios from 'axios';
 
 export default function LoginPage() {
     const [email, setEmail] = useState('');
@@ -12,19 +12,33 @@ export default function LoginPage() {
 
     const handleLogin = async (e) => {
         e.preventDefault();
-
+console.log('err')
         try {
-        const response = await axios.post('/api/auth/login', {
-            email,
-            password,
-        });
+            let data = JSON.stringify({
+                "email": "olajuwonlawal2012@gmail.com",
+                "password": "olajuwon1"
+              });
 
+            //   let config = {
+            //     method: 'post',
+            //     maxBodyLength: Infinity,
+            //     url: 'http://localhost:5000/api/auth/superadmin/login',
+            //     headers: { 
+            //       'Content-Type': 'application/json', 
+            //       'Authorization': 'Basic b2xhanV3b24ubGF3YWxAcG1nbG9iYWx0ZWNobm9sb2d5LmNvbTpQYXNzd29yZA=='
+            //     },
+            //     data : data
+            //   };
+
+            const response  = await axios.post('http://localhost:5000/api/auth/superadmin/login', data).then(res=>res).catch(res => res.response);
+            console.log({response})
+        // const response = await axios.request(config).then(res => res).catch(err=> err);
         if (response.status === 200) {
             console.log(response.data);
             // Handle success, e.g., redirect user, show a success message, etc.
             // window.location.href = '/dashboard'; // Redirect to superadmin dashboard
         } else {
-            setError(response.data.error);
+            setError(response.data.error || response.data.message);
         }
         } catch (error) {
         console.error('Axios error:', error);
